8d81b8a712dd0fbb9af0b95c7aa4d6626e4d841f
AbnnerB <109471209+AbnnerB@users.noreply.github.com>
2023-02-11 12:34:46 -0300

    adicionando mais informações no data

--- src/Data/InfoProjects.js
+++ src/Data/InfoProjects.js
@@ -52,6 +52,38 @@ const InfoProjects = [
     linkVercel: "https://todo-list-pluss.vercel.app/",
     moreInfo: `Nesta lista de Tarefas temos uma função a mais, que é a pagina de detalhes de cada tarefa criada pelo usuário, podendo adicionar Titulos, Listas e parágrafos `,
   },
+  {
+    id: "6",
+    name: "Jogo da Forca",
+    src1: "assets/TodoList1.png",
+    technologiesUsed: "HTML, JS, STYLED-COMPONENTS, REACT,TS ",
+    linkVercel: "https://forca-game-seven.vercel.app/",
+    moreInfo: `Este projeto foi feito seguindo um video do youtube no canal Front-beginners, tendo algumas alterações como pagina inicial, permitindo o usuário escolher a categoria do game, quantidade de vitórias, derrotas entre outras alterações menores`,
+  },
+  {
+    id: "7",
+    name: "Form multi-Step",
+    src1: "assets/TodoList1.png",
+    technologiesUsed: "HTML, JS, STYLED-COMPONENTS, REACT ",
+    linkVercel: "https://multi-step-form-ecru.vercel.app/",
+    moreInfo: `Este projeto é um desafio do front-end Mentor, que como o nome já diz consiste em um formularios com etapas, este projeto foi muito interessante pra mim, pois utilizei o react-hook-form para manipulação dos formularios e tambem consegui colocar em pratica os meus conhecimentos com o hook useContext`,
+  },
+  {
+    id: "8",
+    name: "Countries API",
+    src1: "assets/TodoList1.png",
+    technologiesUsed: "HTML, CSS, JS, REACT ",
+    linkVercel: "https://countries-api-self.vercel.app/",
+    moreInfo: `Este projeto é um desafio do front-end Mentor, com possibilidades de filtro o usuário pode escolher o pais que deseja ver as informações de acordo com o continente ou escrevendo o nome do pais na caixa de pesquisa, contem paginas dinamicas com mais informações sobre o pais selecionado, o usuário tambem pode escolher entre o modo dark-mode ou light-mode  `,
+  },
+  {
+    id: "9",
+    name: "Pokemon API",
+    src1: "assets/TodoList1.png",
+    technologiesUsed: "HTML, STYLED-COMPONENTS, JS, REACT ",
+    linkVercel: "https://pokemon-api-abnnerb.vercel.app/",
+    moreInfo: `Este projeto se trata do consumo de uma api referente a um desenho, contem tambem uma pagina unica de detalhes com mais informações sobre o personagem selecionado. `,
+  },
 ];
 
 export default InfoProjects;
